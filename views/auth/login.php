9f271a5be2d1f61c6466f9d44ecf45e61d52182a
Eduard <eduardbadoyan@gmail.com>
2024-01-05 16:24:05 +0100

    Estilos mixins BEM paginas titulo y formulario

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,1 +1,35 @@
-<h1><?php echo $titulo; ?></h1>
+<main class="auth">
+    <h2 class="auth__heading"><?php echo $titulo; ?></h2>
+    <p class="auth__texto">Inicia sessión en DevCamp</p>
+
+    <form action="" class="formulario">
+        <div class="formulario__campo">
+            <label for="email" class="formulario__label">Email</label>
+            <input
+                type="email"
+                class="formulario__input"
+                placeholder="Tu Email"
+                id="email"
+                name="email"
+            >
+        </div>
+        <div class="formulario__campo">
+            <label for="password" class="formulario__label">Contraseña</label>
+            <input
+                type="password"
+                class="formulario__input"
+                placeholder="Tu Contraseña"
+                id="password"
+                name="password"
+            >
+        </div>
+
+        <input type="submit" class="formulario__submit" value="Iniciar Sessón">
+    </form>
+
+    <div class="acciones">
+        <a href="/registro" class="acciones__enlace">¿Aun no tienes cuenta?</a>
+        <a href="/registro" class="acciones__enlace">¿Olvitaste la contraseña?</a>
+    </div>
+
+</main>
